c1fbfb508b84247f0ca6201b9d1c2ed09e46cee2
David-B-A <dabelloa@unal.edu.co>
2019-11-18 14:12:15 -0500

    First commit - stitching working with OpenCV 3.2.0

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,20 +3,52 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/stitching.hpp"
 
 using namespace std;
 using namespace cv;
 
 extern "C"
 {
-void JNICALL Java_ch_hepia_iti_opencvnativeandroidstudio_MainActivity_salt(JNIEnv *env, jobject instance,
-                                                                           jlong matAddrGray,
-                                                                           jint nbrElem) {
-    Mat &mGr = *(Mat *) matAddrGray;
+void JNICALL Java_ch_hepia_iti_opencvnativeandroidstudio_MainActivity_processPanorama(
+        JNIEnv * env, jclass clazz, jlongArray imageAddressArray, jlong outputAddress) {
+
+    Stitcher::Mode mode = Stitcher::PANORAMA;
+
+    // Get the length of the long array
+    jsize a_len = env->GetArrayLength(imageAddressArray);
+    // Convert the jlongArray to an array of jlong
+    jlong *imgAddressArr = env->GetLongArrayElements(imageAddressArray,0);
+    // Create a vector to store all the image
+    vector< Mat > imgVec;
+    for(int k=0;k<a_len;k++)
+    {
+        // Get the image
+        Mat & curimage=*(Mat*)imgAddressArr[k];
+        Mat newimage;
+        // Convert to a 3 channel Mat to use with Stitcher module
+        cvtColor(curimage, newimage, CV_BGRA2RGB);
+        // Reduce the resolution for fast computation
+        float scale = 1000.0f / curimage.rows;
+        resize(newimage, newimage, Size(scale * curimage.rows, scale *
+                                                               curimage.cols));
+        imgVec.push_back(newimage);
+    }
+    Mat & result = *(Mat*) outputAddress;
+    Stitcher stitcher = Stitcher::createDefault();
+    stitcher.stitch(imgVec, result);
+    // Release the jlong array
+    env->ReleaseLongArrayElements(imageAddressArray, imgAddressArr ,0);
+
+    /* Mat &mGr = *(Mat *) matAddrGray;
     for (int k = 0; k < nbrElem; k++) {
         int i = rand() % mGr.cols;
         int j = rand() % mGr.rows;
         mGr.at<uchar>(j, i) = 255;
     }
+     */
+
 }
 }
--- app/src/main/java/ch/hepia/iti/opencvnativeandroidstudio/MainActivity.java
+++ app/src/main/java/ch/hepia/iti/opencvnativeandroidstudio/MainActivity.java
@@ -1,26 +1,59 @@
 package ch.hepia.iti.opencvnativeandroidstudio;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "OCVSample::Activity";
     private CameraBridgeViewBase _cameraBridgeViewBase;
+    private Button captureBtn, saveBtn;
+    private SurfaceView mSurfaceView, mSurfaceViewOnTop;
+    private Camera mCam;
+    private boolean isPreview;
+    private boolean safeToTakePicture = true;
+    private List<Mat> listImage = new ArrayList<>();
+    ProgressDialog ringProgressDialog;
 
+    static {
+        //System.loadLibrary("opencv_java3");
+        System.loadLibrary("native-lib");
+    }
+    /*
     private BaseLoaderCallback _baseLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -40,32 +73,192 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         }
     };
 
+     */
+
+    View.OnClickListener captureOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(mCam != null && safeToTakePicture){
+                safeToTakePicture = false;
+                mCam.takePicture(null, null, jpegCallback);
+            }
+        }
+    };
+    View.OnClickListener saveOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Thread thread = new Thread(imageProcessingRunnable);
+            thread.start();
+        }
+    };
+    private Runnable imageProcessingRunnable = new Runnable() {
+        @Override
+        public void run() {
+            showProcessingDialog();
+            // implement OpenCV parts
+            try {
+                // Create a long array to store all image address
+                int elems= listImage.size();
+                long[] tempobjadr = new long[elems];
+                for (int i=0;i<elems;i++){
+                    tempobjadr[i]= listImage.get(i).getNativeObjAddr();
+                }
+                // Create a Mat to store the final panorama image
+                Mat result = new Mat();
+                // Call the OpenCV C++ Code to perform stitching process
+                processPanorama(tempobjadr, result.getNativeObjAddr());
+                // Save the image to external storage
+                File sdcard = Environment.getExternalStorageDirectory();
+                final String fileName = sdcard.getAbsolutePath() + "/opencv_" +
+                        System.currentTimeMillis() + ".png";
+                Imgcodecs.imwrite(fileName, result);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "File saved at: " +
+                                fileName, Toast.LENGTH_LONG).show();
+                    }
+                });
+                listImage.clear();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            closeProcessingDialog();
+        }
+    };
+    SurfaceHolder.Callback mSurfaceCallback = new SurfaceHolder.Callback(){
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                mCam.setPreviewDisplay(holder);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                                   int height) {
+            Camera.Parameters myParameters = mCam.getParameters();
+            Camera.Size myBestSize = getBestPreviewSize( myParameters );
+            if(myBestSize != null){
+                myParameters.setPreviewSize(myBestSize.width,
+                        myBestSize.height);
+                mCam.setParameters(myParameters);
+                mCam.setDisplayOrientation(90);
+                mCam.startPreview();
+                isPreview = true;
+            }
+        }
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+        }
+    };
+    private Camera.Size getBestPreviewSize(Camera.Parameters parameters){
+        Camera.Size bestSize = null;
+        List<Camera.Size> sizeList = parameters.getSupportedPreviewSizes();
+        bestSize = sizeList.get(0);
+        for(int i = 1; i < sizeList.size(); i++){
+            if((sizeList.get(i).width * sizeList.get(i).height) >
+                    (bestSize.width * bestSize.height)){
+                bestSize = sizeList.get(i);
+            }
+        }
+        return bestSize;
+    }
+
+    Camera.PictureCallback jpegCallback = new Camera.PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            Matrix matrix = new Matrix();
+            matrix.postRotate(90);
+            bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(),
+                    bitmap.getHeight(), matrix, false);
+            // Save the image to a List to pass them to OpenCV method
+            Mat mat = new Mat();
+            Utils.bitmapToMat(bitmap, mat);
+            listImage.add(mat);
+
+            Canvas canvas = null;
+            try {
+                canvas = mSurfaceViewOnTop.getHolder().lockCanvas(null);
+                synchronized (mSurfaceViewOnTop.getHolder()) {
+                    // Clear canvas
+                    canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+                    // Scale the image to fit the SurfaceView
+                    float scale = 1.0f * mSurfaceView.getHeight() / bitmap.getHeight();
+                    Bitmap scaleImage = Bitmap.createScaledBitmap(bitmap, (int) (scale * bitmap.getWidth()), mSurfaceView.getHeight() , false);
+                    Paint paint = new Paint();
+                    // Set the opacity of the image
+                    paint.setAlpha(200);
+                    // Draw the image with an offset so we only see one third of image.
+                    canvas.drawBitmap(scaleImage, -scaleImage.getWidth() * 2 / 3, 0, paint);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (canvas != null) {
+                    mSurfaceViewOnTop.getHolder().unlockCanvasAndPost(canvas);
+                }
+            }
+            // Start preview the camera again and set the take picture flag to true
+            mCam.startPreview();
+            safeToTakePicture = true;
+        }
+    };
+
+    private void showProcessingDialog(){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mCam.stopPreview();
+                ringProgressDialog = ProgressDialog.show(MainActivity.this, "",
+                        "Panorama", true);
+                ringProgressDialog.setCancelable(false);
+            }
+        });
+    }
+    private void closeProcessingDialog(){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mCam.startPreview();
+                ringProgressDialog.dismiss();
+            }
+        });
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
-
-        // Permissions for Android 6+
-        ActivityCompat.requestPermissions(MainActivity.this,
-                new String[]{Manifest.permission.CAMERA},
-                1);
-
-        _cameraBridgeViewBase = (CameraBridgeViewBase) findViewById(R.id.main_surface);
-        _cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
-        _cameraBridgeViewBase.setCvCameraViewListener(this);
+        isPreview = false;
+        mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        mSurfaceView.getHolder().addCallback(mSurfaceCallback);
+        mSurfaceViewOnTop = (SurfaceView) findViewById(R.id.surfaceViewOnTop);
+        mSurfaceViewOnTop.setZOrderOnTop(true); // necessary
+        mSurfaceViewOnTop.getHolder().setFormat(PixelFormat.TRANSPARENT);
+        captureBtn = (Button) findViewById(R.id.capture);
+        captureBtn.setOnClickListener(captureOnClickListener);
+        saveBtn = (Button) findViewById(R.id.save);
+        saveBtn.setOnClickListener(saveOnClickListener);
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        disableCamera();
+        if (isPreview) {
+            mCam.stopPreview();
+        }
+        mCam.release();
+        mCam = null;
+        isPreview = false;
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        /*
         if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, _baseLoaderCallback);
@@ -73,6 +266,9 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
             Log.d(TAG, "OpenCV library found inside package. Using it!");
             _baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
+
+         */
+        mCam = Camera.open(0);
     }
 
     @Override
@@ -96,28 +292,14 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         }
     }
 
-    public void onDestroy() {
-        super.onDestroy();
-        disableCamera();
-    }
-
-    public void disableCamera() {
-        if (_cameraBridgeViewBase != null)
-            _cameraBridgeViewBase.disableView();
-    }
-
-    public void onCameraViewStarted(int width, int height) {
-    }
-
-    public void onCameraViewStopped() {
-    }
-
+    /*
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         Mat matGray = inputFrame.gray();
         salt(matGray.getNativeObjAddr(), 2000);
         return matGray;
     }
+    */
 
-    public native void salt(long matAddrGray, int nbrElem);
+    public native static void processPanorama(long[] imageAddressArray, long outputAddress);
 }
 
